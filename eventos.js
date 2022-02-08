1b88bc5a1994cb3c637d0b3b086f40483f9ac5b7
Wilson-Carvalho <wilson.carvalho2543@gmail.com>
2022-02-08 20:38:15 -0300

    Sistema de cadastro de eventos

---
+++ eventos.js
@@ -0,0 +1,46 @@
+// Cadastro de participantes em eventos
+// Cadastro no evento, data: 09/02/2022
+let dataedoevento= 09/02/2022
+   if(dataedoevento > 08/02/2022) {
+       console.log("data valida")
+   } else {
+       console.log("cadastro não realizado, data invalida")
+   }
+// Cadastro no evento, idade:
+let idadedoparticipante = 20
+     if(idadedoparticipante >= 18) {
+         console.log("cadastro realizado, idade aceita")
+     } else {
+         console.log("cadastro não realizado, idade inferior a 18")
+     }
+// Cadastro do participante por evento, idade:
+let eventoA= 90
+let eventoB=101
+let eventoC=99
+    if(eventoA<100){
+        console.log("Cadastro realizado no eventoA")
+    } else {
+        console.log("Cadastro não realizado no eventoA")
+    }
+    if(eventoB<100){
+        console.log("Cadastro realizado no eventoB")
+    } else {
+        console.log("Cadastro não realizado no eventoB")
+    }
+    if(eventoC<100){
+        console.log("Cadastro realizado no eventoC")
+    } else {
+        console.log("Cadastro não realizado no eventoC")
+    }
+
+// Criando uma lista de participantes
+let listaParticipantes = ['']
+         console.log(listaParticipantes.length)
+
+// Criando uma lista de palestrantes
+let listaPalestrantes = ['']
+         console.log(listaPalestrantes.length)
+
+
+
+    
